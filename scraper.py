f90c9969a534c3fa2bdc9be8516a340ffaac42dc
Skarf <adiamar@Adis-Mac.local>
2020-08-20 04:44:01 +0300

    Files changing

--- scraper.py
+++ scraper.py
@@ -3,14 +3,18 @@ from bs4 import BeautifulSoup
 import smtplib
 import time
 
-URL = 'https://www.microsoft.com/en-il/p/lego-marvel-super-heroes-2/c0g91v37489h?activetab=pivot:overviewtab'
+
+# Random Website URL to Scrape:
+URL = 'https://www.microsoft.com/en-us/p/lego-marvel-super-heroes-2/c0g91v37489h?activetab=pivot:overviewtab'
 
 headers = {
-    "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
+    "User-Agent": 'YOUR_USER_AGENT'}
 
 
 def check_price():
     global converted_price
+
+    exp_price = 0 # Change to whatever you want
 
     page = requests.get(URL, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -19,7 +23,7 @@ def check_price():
     price = soup.find(id="ProductPrice_productPrice_PriceContainer").get_text()
     converted_price = float(price[-6:-1])
 
-    if (converted_price > 92.87):
+    if (converted_price > exp_price):
         send_mail()
 
     print(converted_price)
@@ -33,14 +37,14 @@ def send_mail():
     server.starttls()
     server.ehlo()
 
-    render_email = 'adiamar2005@gmail.com'
-    render_pass = 'AmarAdi19122005'
-    receiver_email = 'skarf.business@gmail.com'
+    render_email = 'RENDER_EMAIL@gmail.com'
+    render_pass = 'RENDER_EMAIL_PASSWORD'
+    receiver_email = 'RECEIVER_EMAIL@gmail.com'
 
     server.login(render_email, render_pass)
 
-    subject = "Price Fell Down!"
-    body = f"The price of LEGO Marvel Super Heroes 2 is now {converted_price}.\nCheck out the game link if you would like to purchase: {URL}"
+    subject = "YOUR_SUBJECT"
+    body = f"YOUR_BODY"
     msg = f"Subject: {subject}\n\n{body}"
 
     server.sendmail(
